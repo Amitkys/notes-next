7e8822aa65cef6dab26657abcd180fd8dd0f52af
KYs <amitkys59@gmail.com>
2024-10-05 08:38:05 +0530

    undo change in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {
@@ -15,8 +14,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
+  plugins: [],
 };
 export default config;
