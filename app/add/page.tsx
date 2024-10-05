e33c341598a6c241cc7d51303778cbb11247783b
KYs <amitkys59@gmail.com>
2024-10-05 15:42:47 +0530

    added href in attribute

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,4 +1,8 @@
+import { useRef } from "react"
+
 export default function add() {
+    const titleRef = useRef<HTMLInputElement>(null);
+    const contentRef = useRef<HTMLTextAreaElement>(null);
     return (
         <div>
             <label className="form-control w-full max-w-xs">
@@ -6,6 +10,7 @@ export default function add() {
                     <span className="label-text font-semibold text-[19px]">What is your name?</span>
                 </div>
                 <input
+                    ref={titleRef}
                     type="text"
                     placeholder="Type here"
                     className="input input-md text-lg border-2 border-gray-700 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:border-gray-500 focus:ring-0 focus:ring-gray-500"
@@ -17,10 +22,12 @@ export default function add() {
                     <span className="label-text font-semibold text-[19px]">What is your name?</span>
                 </div>
                 <textarea
+                    ref={contentRef}
                     placeholder="Type here"
-                    className="input input-md text-lg border-2 border-gray-700 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:border-gray-500 focus:ring-0 focus:ring-gray-500"
+                    className="textarea  text-lg border-2 border-gray-700 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:border-gray-500 focus:ring-0 focus:ring-gray-500"
                 />
             </label>
+            <button className="btn btn-sm bg-success text-black mt-3">Add Notes</button>
         </div>
     )
 }
