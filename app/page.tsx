f387b7e26330174bc02ec335eeef5a4512540d91
KYs <amitkys59@gmail.com>
2024-10-05 14:37:50 +0530

    lots of component tried back to daisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div>
       <h1> this is home page</h1>
-      <button className="btn">hello</button>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,19 +6,9 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  daisyui: {
-    themes: ["dark"],
-  },
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  darkMode: 'class',
   plugins: [
-    require('daisyui'),
+    
   ],
 };
 export default config;
