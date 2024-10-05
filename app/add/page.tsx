793e351cb24bb5d0e2eb39a2860b9d5bf9a1cb3f
KYs <amitkys59@gmail.com>
2024-10-05 15:34:22 +0530

    designed form for inserting text in db

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,7 +1,26 @@
-export default function add(){
-    return(
+export default function add() {
+    return (
         <div>
-            this is adding page
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text font-semibold text-[19px]">What is your name?</span>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-md text-lg border-2 border-gray-700 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:border-gray-500 focus:ring-0 focus:ring-gray-500"
+                />
+            </label>
+            {/* 2nd labe  */}
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text font-semibold text-[19px]">What is your name?</span>
+                </div>
+                <textarea
+                    placeholder="Type here"
+                    className="input input-md text-lg border-2 border-gray-700 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:border-gray-500 focus:ring-0 focus:ring-gray-500"
+                />
+            </label>
         </div>
     )
 }
