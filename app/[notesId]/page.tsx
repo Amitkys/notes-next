4309be072c057de17442c1bfbe23af49de7634ca
KYs <amitkys59@gmail.com>
2024-10-06 06:50:40 +0530

    showing indivual data

--- app/[notesId]/page.tsx
+++ app/[notesId]/page.tsx
@@ -1,7 +1,48 @@
-export default function notesId({ params }: { params: { notesId: string } }){
+import client from "@/lib/Singleton_prisma";
+
+async function fetchData(id: string) {
+    const data = await client.note.findUnique({
+        where: { id: id },
+    });
+    return data;
+}
+
+function formattedDate(updatedAt: Date) {
+    const date = new Date(updatedAt);
+
+    const year = date.getFullYear();
+    const month = date.toLocaleString('default', { month: 'short' }); // Get short month name
+    const day = date.getDate();
+
+    // Use locale string to get 12-hour time format with AM/PM
+    const time = date.toLocaleString('default', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true, // Enable 12-hour format
+    });
+
+    // Return the formatted string
+    return `${year}, ${day} ${month}: ${time}`;
+}
+
+
+export default async function notesId({ params }: { params: { notesId: string } }) {
+    const data = await fetchData(params.notesId);
+
+    if (!data) {
+        return (
+            <div>
+                <p>Note not found</p>
+            </div>
+        );
+    }
+
     return (
         <div>
-            <h1>fetch data for { params.notesId }</h1>
+            <p>Last update: {formattedDate(data.updatedAt)}</p>
+            <h1>Title: {data.title}</h1>
+            <h1>Notes: {data.content}</h1>
         </div>
-    )
+    );
 }
